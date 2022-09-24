91776c0aa4857d1cece22726943fde596ab924a6
delucca <gpg@delucca.dev>
2022-09-24 12:03:35 -0300

    finishing base application

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # MO837 Dask
 
 This repository is an exploration over Dask for UNICAMP's MO837 class. It is organized into different branches, where each branch represents a part of the exploration itself.
+
+## Getting started
+
+This application executes a Dask workflow using a distributed cluster. In this section you'll learn how to setup the dependencies, install the application requirements, launch the cluster, and execute the application
+
+### Installing dependencies
+
+To execute the application you need to install the following dependencies:
+
+- [poetry](https://python-poetry.org/docs/#installation)
+
+### Installing application requirements
+
+After installing all dependencies, you can run the followingg command (at the root folder of this repository):
+
+```bash
+poetry install
+```
+
+
+This will install all Python requirements for this application
+
+### Launching the cluster
+
+With the requirements installed, you can run the following command to launch the local cluster:
--- src/app.py
+++ src/app.py
@@ -1,17 +1,36 @@
-import h5py
-import dask.array as da
+import time
 
-from blaze import data, into
+from argparse import ArgumentParser
+from dask import array as da
+from dask.distributed import Client
 
-f = h5py.File('myfile.hdf5', 'a')
-A = f.create_dataset(name='A', shape=(200000, 4000), dtype='f8',
-                     chunks=(250, 250), fillvalue=1.0)
-B = f.create_dataset(name='B', shape=(4000, 4000), dtype='f8',
-                     chunks=(250, 250), fillvalue=1.0)
 
-a = into(da.from_array(), 'myfile.hdf5::/A', blockshape=(1000, 1000))
-b = into(da.from_array(), 'myfile.hdf5::/B', blockshape=(1000, 1000))
-A = data(a)
-B = data(b)
+def main(scheduler_url, size, chunk_size):
+  if scheduler_url: launch_distributed_client(scheduler_url)
 
-into('myfile.hdf5::/result', A.dot(B))
+  print(f'Calculating the matrix multiplication of 2 ({size},{size}) matrices, divided into chunks of {chunk_size}')
+
+  a = da.random.random((size, size), chunks=(chunk_size, chunk_size))
+  b = da.random.random((size, size), chunks=(chunk_size, chunk_size))
+
+  start_time = time.perf_counter()
+  a.dot(b).compute()
+  end_time = time.perf_counter()
+
+  elapsed_time = end_time - start_time
+  print('Compute time: {0:.2f} seconds'.format(elapsed_time))
+
+def launch_distributed_client(scheduler_url):
+  print(f'Using the scheduler located at: {scheduler_url}')
+  client = Client(scheduler_url)
+
+
+if __name__ == '__main__':
+  parser = ArgumentParser(description='Matmul using Dask')
+  parser.add_argument('--size', type=int, default=20000, help='the size of the matrix (default: 20000)')
+  parser.add_argument('--chunk_size', type=int, default=10000, help='the size of the chunks (default: 10000)')
+  parser.add_argument('--scheduler_url', type=str, help='the scheduler URL to be used')
+
+  args = parser.parse_args()
+
+  main(scheduler_url=args.scheduler_url, size=args.size, chunk_size=args.chunk_size)
